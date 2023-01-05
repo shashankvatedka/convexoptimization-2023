170e2dd491059a1e7006d80994a41e614fc857cb
Shashank <shashankvatedka@ee.iith.ac.in>
2023-01-05 10:48:18 +0530

    initial commit

---
+++ python_basics/helloworld.py
@@ -0,0 +1,8 @@
+# this is a comment
+
+"""
+this is also a comment,
+but multi line
+"""
+
+print("Hello world")
---
+++ python_basics/includeFile.py
@@ -0,0 +1,5 @@
+PI = 3.1416
+
+def computeSphereVolume(radius):
+    volume = 4/3 * PI * (radius**3)
+    return(volume)
